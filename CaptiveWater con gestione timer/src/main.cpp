7082999c7b7b6ba7d414043aaffb02ec5ed6c32b
jago <jago.revrenna@studenti.unitn.it>
2025-02-17 11:01:50 +0100

    rimossa un po' di roba inutile

--- CaptiveWater con gestione timer/src/main.cpp
+++ CaptiveWater con gestione timer/src/main.cpp
@@ -5,63 +5,61 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 #include <ESP32Servo.h>
-//#include <avr/wdt.h>
+// #include <avr/wdt.h>
 #include <esp_system.h>
 
 DNSServer dnsServer;
 AsyncWebServer server(80);
 
-const char* ssid = "distributoreSwag";
-const char* password = NULL;
+const char *ssid = "distributoreSwag";
+const char *password = NULL;
 
-enum AcquaSize {cl25, cl33, cl50, l1};
+enum AcquaSize
+{
+  cl25,
+  cl33,
+  cl50,
+  l1
+};
 
-//led RGB
+// led RGB
 int rossoPin = 19;
 int verdePin = 18;
 int bluPin = 17;
 
-//DISPLAY
+// DISPLAY
 int lcdColumns = 16;
 int lcdRows = 2;
 // Create an LCD object with the I2C address, columns, and rows
 LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
 
-//VARIABILI UTRASUONI 
+// VARIABILI UTRASUONI
 bool erogazioneInCorso = false;
-float acquaErogata = 0.0; // Quantità d'acqua erogata in millilitri
-unsigned long tempoInizioErogazione = 0;
-const unsigned long timeoutRipresa = 5000; // 5 secondi per riposizionare la borraccia
-const int trigPin = 5;     // Pin trigger sensore ultrasuoni
-const int echoPin = 4;    // Pin echo sensore ultrasuoni
-const int servoPin = 2;   // Pin del servomotore
-const float minDistance = 0.0;  // Distanza minima in cm
+const int trigPin = 5;         // Pin trigger sensore ultrasuoni
+const int echoPin = 4;         // Pin echo sensore ultrasuoni
+const int servoPin = 2;        // Pin del servomotore
+const float minDistance = 0.0; // Distanza minima in cm
 const float maxDistance = 5.0; // Distanza massima in cm
 
+// SERVO
 Servo servo;
-const int servoChiuso=0;
-const int servoAperto=90;
+const int servoChiuso = 0;
+const int servoAperto = 90;
 
-
-bool scelta_effettuata = false;   
+// VARIABILI PER GESTIONE SCELTA ACQUA
+bool scelta_effettuata = false;
 AcquaSize acqua_scelta;
 
+// TEMP0
+const long int tempo_25cl = 10000;
+const long int tempo_33cl = 15000;
+const long int tempo_50cl = 20000;
+const long int tempo_1l = 40000;
 
-//dichiarazione timer
-//int timerInizio = 0;
-//int tempoPassato = 0;
-
-const long int tempo_25cl=10000;
-const long int tempo_33cl=15000;
-const long int tempo_50cl=20000;
-const long int tempo_1l=40000;
-
-unsigned long int startTime;        
-unsigned long int elapsedTime=0;     
-
+unsigned long int startTime;
+unsigned long int elapsedTime = 0;
 
 bool timerRunning = false;
-
 
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE html>
@@ -135,71 +133,86 @@ const char index_html[] PROGMEM = R"rawliteral(
 )rawliteral";
 unsigned long int getElapsedTime();
 
-void startTimer() {
-  startTime = millis();     
-  timerRunning = true;       
+void startTimer()
+{
+  startTime = millis();
+  timerRunning = true;
   Serial.println("Timer partito!");
   Serial.println(getElapsedTime());
 }
-void stopTimer() {
-  if (timerRunning) {
-    elapsedTime += millis() - startTime; 
-    timerRunning = false;      
+void stopTimer()
+{
+  if (timerRunning)
+  {
+    elapsedTime += millis() - startTime;
+    timerRunning = false;
     Serial.println("Timer fermato!");
   }
 }
-void restartTimer() {
-  if (!timerRunning) {
-    startTime = millis() - elapsedTime;  // Riprendi da dove era stato interrotto
-    timerRunning = true;                 // Rende il timer attivo
+void restartTimer()
+{
+  if (!timerRunning)
+  {
+    startTime = millis() - elapsedTime; // Riprendi da dove era stato interrotto
+    timerRunning = true;                // Rende il timer attivo
     Serial.println("Timer riavviato!");
   }
 }
 
-void resetTimer() {
+void resetTimer()
+{
   stopTimer();
-  startTime = 0;    // Reset del tempo di inizio
-  elapsedTime = 0;  // Reset del tempo trascorso
+  startTime = 0;   // Reset del tempo di inizio
+  elapsedTime = 0; // Reset del tempo trascorso
   Serial.println("Timer resettato!");
 }
 
-unsigned long int getElapsedTime() {
-  if (timerRunning) {
+unsigned long int getElapsedTime()
+{
+  if (timerRunning)
+  {
     return millis() - startTime; // Calcola il tempo trascorso
-  } else {
+  }
+  else
+  {
     return 0; // Se il timer non è attivo, restituisce 0
   }
 }
 
-
-void rgb(int rosso, int verde, int blu){
+void rgb(int rosso, int verde, int blu)
+{
   analogWrite(rossoPin, rosso);
   analogWrite(verdePin, verde);
   analogWrite(bluPin, blu);
 }
 
-//funzione display
-void scriviDisplay(String linea1, String linea2){
+// funzione display
+void scriviDisplay(String linea1, String linea2)
+{
 
   lcd.setCursor(0, 0); // Set the cursor to the first column, first row
-  lcd.print(linea1); // Print message on the first row
+  lcd.print(linea1);   // Print message on the first row
   lcd.setCursor(0, 1); // Set the cursor to the first column, second row
-  lcd.print(linea2); // Print message on the second row 
+  lcd.print(linea2);   // Print message on the second row
 }
 
-void apriServo(){
+void apriServo()
+{
   servo.attach(servoPin);
-  for(int posDegrees = 0; posDegrees <= servoAperto; posDegrees++) {
+  for (int posDegrees = 0; posDegrees <= servoAperto; posDegrees++)
+  {
     servo.write(posDegrees);
     Serial.println(servo.read());
     delay(5);
   }
-  //servo.detach();
+  // servo.detach();
 }
 
-void chiudiServo(){
+void chiudiServo()
+{
   servo.attach(servoPin);
-  for(int posDegrees = servoAperto; posDegrees >= servoChiuso; posDegrees--) {
+  for (int posDegrees = servoAperto; posDegrees >= servoChiuso; posDegrees--)
+  {
     servo.write(posDegrees);
     Serial.println(servo.read());
     delay(5);
@@ -207,134 +220,131 @@ void chiudiServo(){
   servo.detach();
 }
 
-void erogazioneFinita(){
+void erogazioneFinita()
+{
   resetTimer();
   scriviDisplay("Finita                ", "l'erogazione               ");
   erogazioneInCorso = false;
   rgb(255, 255, 255);
-  //delay(3000);
+  // delay(3000);
   chiudiServo();
   scelta_effettuata = false;
-  
-  //FAR SUONARE BUZZER PER AVVISARE
+
+  // FAR SUONARE BUZZER PER AVVISARE
   delay(5000);
 
   esp_restart();
 }
 
-void erogaErogazione() {
+void erogaErogazione()
+{
   Serial.print("Acqua scelta:  ");
   Serial.println(acqua_scelta);
 
-  // tempoPassato = millis() - timerInizio;
-
-  // //caso in cui parto dall'inizio
-  // if(tempoPassato = 0){
-  //   timerInizio = millis();
-  // }
-  // else{
-
-  if(getElapsedTime()==0){  // Se il tempo trascorso è 0, allora il timer non è mai partito
+  if (getElapsedTime() == 0)
+  { // Se il tempo trascorso è 0, allora il timer non è mai partito
     startTimer();
   }
-  else if(!timerRunning){ //se il timer non è attivo lo riavvia
+  else if (!timerRunning)
+  { // se il timer non è attivo lo riavvia
     restartTimer();
   }
-    switch (acqua_scelta)
+  switch (acqua_scelta)
+  {
+  case cl25:
+    if (getElapsedTime() >= tempo_25cl - elapsedTime)
     {
-    case cl25:
-      if(getElapsedTime()>= tempo_25cl-elapsedTime){
-        erogazioneFinita();
-      }
-      break;
-    case cl33:
-      if(getElapsedTime() >= tempo_33cl-elapsedTime){
-        erogazioneFinita();
-      }
-      break;
-    case cl50:
-      if(getElapsedTime() >= tempo_50cl-elapsedTime){
-        erogazioneFinita();
-      }
-      break;
-    case l1:
-      if(getElapsedTime() >= tempo_1l-elapsedTime){
-        erogazioneFinita();
-      }
-      break;
-    default:
-      break;
+      erogazioneFinita();
     }
-  
-
+    break;
+  case cl33:
+    if (getElapsedTime() >= tempo_33cl - elapsedTime)
+    {
+      erogazioneFinita();
+    }
+    break;
+  case cl50:
+    if (getElapsedTime() >= tempo_50cl - elapsedTime)
+    {
+      erogazioneFinita();
+    }
+    break;
+  case l1:
+    if (getElapsedTime() >= tempo_1l - elapsedTime)
+    {
+      erogazioneFinita();
+    }
+    break;
+  default:
+    break;
+  }
 
   Serial.print("Erogazione in corso");
   Serial.println(servo.read());
   Serial.println(getElapsedTime());
   erogazioneInCorso = true;
-  //tempoInizioErogazione = millis();
+  // tempoInizioErogazione = millis();
   rgb(0, 255, 0); // LED Verde
-  
-  if(servo.read() < servoAperto-10){
+
+  if (servo.read() < servoAperto - 10)
+  {
     apriServo(); // Posizione per apertura valvola
     delay(50);
   }
 }
-void interrompiErogazione() {
+
+void interrompiErogazione()
+{
   Serial.print("Erogazione non in corso  ");
   Serial.print(elapsedTime);
-  //Serial.println(servo.read());
+  // Serial.println(servo.read());
   erogazioneInCorso = false;
   rgb(255, 255, 0); // LED Giallo
-  if(timerRunning){
+  if (timerRunning)
+  {
     stopTimer();
   }
-  
-  if(servo.read() > servoChiuso+10){
+
+  if (servo.read() > servoChiuso + 10)
+  {
     chiudiServo(); // Chiude l'erogatore
-    
+
     delay(50);
   }
-  
 }
 
-
-
-// void riprendiErogazione(){
-//   erogazioneInCorso = true;
-//   rgb(0, 255, 0); // LED Verde
-//   if(servo.read() != servoAperto){
-//     apriServo(); // Posizione per apertura valvola
-//   }
-// }
-
-class CaptiveRequestHandler : public AsyncWebHandler {
+class CaptiveRequestHandler : public AsyncWebHandler
+{
 public:
   CaptiveRequestHandler() {}
   virtual ~CaptiveRequestHandler() {}
 
-  //ritorna true se la richiesta può essere gestita da questo gestore (sempre)
-  bool canHandle(AsyncWebServerRequest *request){
-    //request->addInterestingHeader("ANY");
+  // ritorna true se la richiesta può essere gestita da questo gestore (sempre)
+  bool canHandle(AsyncWebServerRequest *request)
+  {
+    // request->addInterestingHeader("ANY");
     return true;
   }
 
-  //è quella parte di programma che effettivamente manda ciò che verrà aperto dal captive portal (ossia index_html)
-  void handleRequest(AsyncWebServerRequest *request) {
-    request->send_P(200, "text/html", index_html); 
+  // è quella parte di programma che effettivamente manda ciò che verrà aperto dal captive portal (ossia index_html)
+  void handleRequest(AsyncWebServerRequest *request)
+  {
+    request->send_P(200, "text/html", index_html);
   }
 };
 
-void setupServer(){
-  //in caso di richiesta di apertura della root page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+void setupServer()
+{
+  // in caso di richiesta di apertura della root page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
       request->send_P(200, "text/html", index_html); 
       scelta_effettuata=false;
-      Serial.println("Client Connected");
-  });
-    
-  //in caso di form submission
-  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+      Serial.println("Client Connected"); });
+
+  // in caso di form submission
+  server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
       String inputMessage;
       String inputParam;
       //nota che i parametri sono quelli che sono stati nominati nella parte HTML
@@ -346,14 +356,11 @@ void setupServer(){
         Serial.println(inputMessage);
         scelta_effettuata = true;
       }   
-      request->send(200, "text/html", "Inserisci la bottiglia <br><a href=\"/\">Return to Home Page</a>");
-  });
+      request->send(200, "text/html", "Inserisci la bottiglia <br><a href=\"/\">Return to Home Page</a>"); });
 }
 
-
-
-
-float getDistance() {
+float getDistance()
+{
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
@@ -367,18 +374,11 @@ float getDistance() {
   return distance_cm;
 }
 
-
-// void interrompiErogazione() {
-//   erogazioneInCorso = false;
-//   setLEDColor(255, 255, 0); // LED Giallo
-//   servo.write(0); // Chiude l'erogatore
-// }
-
-
-void setup(){
+void setup()
+{
 
   Serial.print("provaaaaaaaaaa");
-  //your other setup stuff...
+  // your other setup stuff...
   Serial.begin(115200);
   Serial.println();
 
@@ -387,75 +387,43 @@ void setup(){
   pinMode(echoPin, INPUT);
 
   Serial.println("Setting up AP Mode");
-  WiFi.mode(WIFI_AP); 
+  WiFi.mode(WIFI_AP);
   WiFi.softAP(ssid, password, 1, 0, 1);
-  Serial.print("AP IP address: ");Serial.println(WiFi.softAPIP());
+  Serial.print("AP IP address: ");
+  Serial.println(WiFi.softAPIP());
 
   Serial.println("Setting up Async WebServer");
   setupServer();
 
   Serial.println("Starting DNS Server");
   dnsServer.start(53, "*", WiFi.softAPIP());
-  server.addHandler(new CaptiveRequestHandler()).setFilter(ON_AP_FILTER);//only when requested from AP
-  
-  //more handlers...
+  server.addHandler(new CaptiveRequestHandler()).setFilter(ON_AP_FILTER); // only when requested from AP
+
+  // more handlers...
   server.begin();
   Serial.println("All Done!");
 
-
-  //SET UP DISPLAY
-  lcd.init(); // Initialize the LCD
+  // SET UP DISPLAY
+  lcd.init();      // Initialize the LCD
   lcd.backlight(); // Turn on the LCD backlight
 
-  //SET UP SERVO
-  //servo.attach(servoPin);
-  //Serial.print("Servo read:");
-  /*servo.write(servoChiuso);
-  if(servo.read() != servoChiuso){
-    chiudiServo(); 
-  }*/
   chiudiServo();
-  
 }
 
-
-
-void loop(){
-
-  // if(scelta_effettuata){
-  //   if(distance > maxDistance || distance < minDistance){  //se la distanza è compresa tra questo range e la scelta è stata effettuata allora si può erogare 
-  //     switch (acqua_scelta)
-  //     {
-  //     case cl25:
-    
-  //       break;
-  //       case cl33:
-
-  //       break;
-  //       case cl50:
-
-  //       break;
-        
-  //       case l1:
-
-  //       break;
-  //     default:
-  //       break;
-  //     }
-  //   }
-  // }
-
-
+void loop()
+{
   int clientCount = WiFi.softAPgetStationNum();
-  //Serial.println(clientCount);
-  if(clientCount == 0){
+  // Serial.println(clientCount);
+  if (clientCount == 0)
+  {
     rgb(0, 0, 255);
     scriviDisplay("Pronto                ", "collegarsi                ");
     delay(10);
     scelta_effettuata = false;
   }
-  
-  else if(!scelta_effettuata){
+
+  else if (!scelta_effettuata)
+  {
     rgb(255, 0, 255);
     scriviDisplay("Connesso:                ", "scegli taglia                ");
     delay(100);
@@ -463,43 +431,18 @@ void loop(){
 
   dnsServer.processNextRequest();
 
-  if(scelta_effettuata){
+  if (scelta_effettuata)
+  {
     float distance = getDistance();
-    if(distance >= minDistance && distance <= maxDistance){     
+    if (distance >= minDistance && distance <= maxDistance)
+    {
       scriviDisplay("Borraccia                 ", "rilevata                ");
       erogaErogazione();
     }
-    else if(distance <= minDistance || distance >= maxDistance){ 
+    else if (distance <= minDistance || distance >= maxDistance)
+    {
       scriviDisplay("Metti la                ", "borraccia                ");
       interrompiErogazione();
     }
-
-
-
-
-    // if(!erogazioneInCorso){
-    //   rgb(255, 255, 0);
-    //   scriviDisplay("Metti la                ", "borraccia                ");
-    // }
-    // Serial.println("Nel loop scelta_effettuata");
-    
-
-    // if(!erogazioneInCorso && distance >= minDistance && distance <= maxDistance) {
-    //     Serial.println("Borraccia rilevata! Apertura valvola...");
-    //     scriviDisplay("Borraccia                 ", "rilevata                ");
-    //     iniziaErogazione(); // Valvola chiusa inizialmente
-    //     //rgb(0,255,0); //led verde
-    // }
-    // if(erogazioneInCorso && distance <= minDistance && distance >= maxDistance){
-    //   scriviDisplay("Metti la                ", "borraccia                ");
-    //   interrompiErogazione();
-    // }
-    
-    //   delay(100);
-    //   Serial.print("Size: ");Serial.println(acqua_scelta);
-      //Serial.println("We'll wait for the next client now");
   }
-
- 
-  
 }
