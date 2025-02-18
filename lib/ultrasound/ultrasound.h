01c3e6be2fda9586c1d8965251a306c9aca10b80
jago <jago.revrenna@studenti.unitn.it>
2025-02-18 18:58:11 +0100

    Final version

--- lib/ultrasound/ultrasound.h
+++ lib/ultrasound/ultrasound.h
@@ -3,8 +3,8 @@
 
 #define TRIG_PIN 5       // Pin trigger sensore ultrasuoni
 #define ECHO_PIN 4         // Pin echo sensore ultrasuoni
-#define MIN_DISTANCE 0.0 // Distanza minima in cm
-#define MAX_DISTANCE 5.0 // Distanza massima in cm
+#define MIN_DISTANCE 5.0 // Distanza minima in cm
+#define MAX_DISTANCE 10.0 // Distanza massima in cm
 
 float getDistance();
 
