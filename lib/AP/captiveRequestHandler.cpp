eccff0c3cd063fa34d2e44ba1c5ef7638b2c196a
renna3 <63052880+renna3@users.noreply.github.com>
2025-02-19 11:59:02 +0100

    Update captiveRequestHandler.cpp

--- lib/AP/captiveRequestHandler.cpp
+++ lib/AP/captiveRequestHandler.cpp
@@ -4,11 +4,13 @@ CaptiveRequestHandler::CaptiveRequestHandler() {}
 
 CaptiveRequestHandler::~CaptiveRequestHandler() {}
 
+// this function returns true for all the requests
 bool CaptiveRequestHandler::canHandle(AsyncWebServerRequest *request)
 {
     return true;
 }
 
+// this function is the one that actually sends what will be opened by the captive portal (i.e. index_html)
 void CaptiveRequestHandler::handleRequest(AsyncWebServerRequest *request)
 {
     request->send_P(200, "text/html", index_html);
